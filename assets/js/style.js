e86c69d235f463323e194241bc5f047db6f3bda8
Zeeshan Mahmood <zeeshan.mahmood1997@gmail.com>
2024-08-27 15:30:58 +0000

    add referneces to the html in js using const

--- assets/js/style.js
+++ assets/js/style.js
@@ -3,3 +3,12 @@ let player2Health = 100;
 let player1SpecialAttackUsed = false;
 let player2SpecialAttackUsed = false;
 
+const player1 = document.getElementById('player1');
+const player2 = document.getElementById('player2');
+const player1HealthDisplay = document.getElementById('player1-health');
+const player2HealthDisplay = document.getElementById('player2-health');
+const player1AttackBtn = document.getElementById('player1-attack');
+const player1SpecialAttackBtn = document.getElementById('player1-special-attack');
+const gameMessage = document.getElementById('game-message');
+const restartBtn = document.getElementById('restart-button');
+
